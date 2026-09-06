2e6c206d4619ad031f5c92c029dd82052aaddae9
TAF Fuwad <fuwadmd555@gmail.com>
2026-09-06 13:17:10 +0600

    Add animations to page 2 heading with GSAP and ScrollTrigger

--- Project-Hub/pro.js
+++ Project-Hub/pro.js
@@ -13,3 +13,54 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+// Sweep the page 2 heading across the screen on a soft, curved path.
+gsap.registerPlugin(ScrollTrigger);
+
+const page2 = document.querySelector('.page2');
+const page2Heading = document.querySelector('.page2 .text-anim h1');
+
+if (page2 && page2Heading) {
+    const viewportWidth = () => window.innerWidth;
+    const exitPosition = () => -page2Heading.scrollWidth - viewportWidth() * 0.2;
+
+    gsap.set(page2Heading, {
+        x: () => viewportWidth() * 1.1,
+        y: '8vh',
+        rotation: 8,
+        transformOrigin: '50% 50%'
+    });
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: page2,
+            start: 'top top',
+            end: '+=250%',
+            pin: true,
+            scrub: 1.5,
+            anticipatePin: 1,
+            invalidateOnRefresh: true
+        }
+    })
+        .to(page2Heading, {
+            x: () => viewportWidth() * 0.4,
+            y: '-11vh',
+            rotation: -3,
+            duration: 0.5,
+            ease: 'none'
+        })
+        .to(page2Heading, {
+            x: () => -viewportWidth() * 0.55,
+            y: '10vh',
+            rotation: 3,
+            duration: 0.5,
+            ease: 'none'
+        })
+        .to(page2Heading, {
+            x: exitPosition,
+            y: '-5vh',
+            rotation: -2,
+            duration: 0.5,
+            ease: 'none'
+        });
+}
